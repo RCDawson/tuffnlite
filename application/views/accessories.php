13594e834cf9d9aeec80c5caa88a21c363629067
dhx816 <ryan.dawson@capitalone.com>
2014-07-24 07:33:09 -0400

    Accessories - started

--- application/views/accessories.php
+++ application/views/accessories.php
@@ -1,1 +1,51 @@
-<div>Accessories page</div>
+<div class="row">
+    <div id="main">
+        <div role="presentation" id="hero">
+            <div class="column four">
+                <div>
+                    <h2 style="line-height:1.25; padding-bottom:.35em">Aluminum Form</h2>
+                    <h3>Hardware Accessories</h3>
+                    <h4>Helps to increase labor productivity</h4>
+                </div>
+            </div>
+            <div class="column eight">
+                <img src="/assets/images/hero-accessories.jpg">
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<div role="presentation" class="row">
+    <div class="spotlight">
+        <h4>Curved Wedge<br>Standard Wedge</h4>
+        <img src="/assets/images/accessories/wedges.jpg">
+        <ul>
+            <li>Hardened for many reuses</li>
+            <li>Packed in buckets of 250</li>
+            <li>Weight 26lbs.</li>
+        </ul>
+    </div>
+    <div class="spotlight">
+        <h4>Standard Pin<br>&nbsp;</h4>
+        <img src="/assets/images/accessories/standard-pin.jpg">
+        <ul>
+            <li>Hardened for many reuses</li>
+            <li>Packed in buckets of 250</li>
+            <li>Weight 53lbs.</li>
+        </ul>
+    </div>
+    <div class="spotlight">
+        <h4>45&deg; Angle Clip<br>&nbsp;</h4>
+        <img src="/assets/images/accessories/angle-clip-45.jpg">
+        <ul>
+            <li>Hardened for many reuses</li>
+            <li>Packed in buckets of 250</li>
+            <li>Weight 26lbs.</li>
+        </ul>
+    </div>
+    <div class="spotlight">
+        <h4>Aluminum Tie Pin, Angle Clip and Cap Clip</h4>
+        <img src="/assets/images/forms/spotlight-fpo.jpg">
+        <p>Optional Ready Pin attached hardware is durable, lightweight and increases labor productivity, while reducing lost hardware expenses.</p>
+    </div>
+</div>
