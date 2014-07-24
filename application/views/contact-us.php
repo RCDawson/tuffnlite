eb8c1a1b873da939dd930ee4d13133e4d160d04f
dhx816 <ryan.dawson@capitalone.com>
2014-07-24 07:33:38 -0400

    Contact Us - v2

--- application/views/contact-us.php
+++ application/views/contact-us.php
@@ -1,1 +1,44 @@
-<div>Contact Us page</div>
+<div class="row">
+    <div id="main">
+        <div role="presentation" id="hero">
+            <div class="column four">
+                <div>
+                    <h2>TUF-N-LITE</h2>
+                    <p>4805 Hamilton Middletown Rd<br>
+                        Liberty Township, OH 45011
+                    </p>
+                    <h3>(513) 422-5419</h3>
+                    <h3><a href="#">Email Us</a></h3>
+                </div>
+            </div>
+            <div class="column eight">
+                <a target="_blank" href="https://www.google.com/maps/place/4805+Hamilton+Middletown+Rd,+Liberty+Township,+OH+45011/@39.424374,-84.455455,12z">
+                    <img src="/assets/images/googlemap-642-365-12zoom.png">
+                </a>
+<!--                <img src="http://maps.googleapis.com/maps/api/staticmap?center=4805+Hamilton+Middletown+Rd,+Liberty+Township,+OH+45011&zoom=15&markers=label:|4805+Hamilton+Middletown+Rd,+Liberty+Township,+OH+45011&size=642x365&scale=2" style="max-height: 365px">-->
+            </div>
+        </div>
+    </div>
+</div>
+<?php /*
+<p style="color: #666666; font-style: italic">&nbsp;</p>
+<p style="color: #666666; font-style: italic">&nbsp;</p>
+<p style="color: #666666; font-style: italic">(option 2)</p>
+<div class="row">
+    <div class="column three offset-by-four">
+        <h2>TUF-N-LITE</h2>
+        <p>4805 Hamilton Middletown Rd<br>
+            Liberty Township, OH 45011
+        </p>
+        <h3>(513) 422-5419</h3>
+        <h3><a href="#" id="emailUs" style="text-decoration:none">Email Us</a></h3>
+    </div>
+    <div class="column five">
+        <p><a target="_blank" href="https://maps.google.com/maps/place/4805+Hamilton+Middletown+Rd,+Liberty+Township,+OH+45011">
+<!--                <img src="http://maps.googleapis.com/maps/api/staticmap?center=4805+Hamilton+Middletown+Rd,+Liberty+Township,+OH+45011&zoom=15&markers=label:|4805+Hamilton+Middletown+Rd,+Liberty+Township,+OH+45011&size=350x350&scale=2"  style="border:3px solid #225588; width:96%; border-radius:10px">-->
+                <img src="/assets/images/googlemap.png"  style="border:3px solid #225588; display:block; width:96%; border-radius:10px">
+            </a>
+        </p>
+    </div>
+</div>
+*/ ?>
