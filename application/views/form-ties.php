b4fc91742479340028578bd6256736d26b01ab3a
dhx816 <ryan.dawson@capitalone.com>
2014-07-24 07:32:04 -0400

    Form-ties - v1

--- application/views/form-ties.php
+++ application/views/form-ties.php
@@ -1,1 +1,26 @@
-<div>Form ties page</div>
+<div class="row">
+    <div id="main">
+        <div role="presentation" id="hero">
+            <div class="column four">
+                <div>
+                    <h2 style="line-height: 1.25; padding-bottom: .25em">Independent Test Results</h2>
+                    <h3>Calculated pressures on each tie once placed at different heights</h3>
+                    <h4>PROTECT YOUR EMPLOYEES!</h4>
+                </div>
+            </div>
+            <div class="column eight">
+                <img src="/assets/images/hero-form-ties.png">
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<div role="presentation" class="row">
+    <div class="column eight">
+        <h1>The Standard Has Been Set</h1>
+        <p>Protect your employees, as well as your company, by avoiding potential injuries from using cheaper ties NOT made in the USA.</p>
+    </div>
+    <div class="column four">
+        <blockquote>Our products were developed by concrete contractors for concrete contractors.</blockquote>
+    </div>
+</div>
