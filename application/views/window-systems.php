c15ef3cfabb56686a164be03d97acd8cd044d6b0
Ryan <iamryandawson@gmail.com>
2014-07-25 10:02:41 -0400

    Window Systems - initial build

--- application/views/window-systems.php
+++ application/views/window-systems.php
@@ -1,1 +1,48 @@
-<div>Window Systems page</div>
+<div class="row">
+    <div id="main">
+        <div role="presentation" id="hero">
+            <div class="column four">
+                <div>
+                    <h2 class="smaller">Snap-In-Place</h2>
+                    <h2 class="smaller" style="border:none">Window Systems</h2>
+                    <h3 style="font-size: 1.5em">Safe and Secure Basement Window Systems</h3>
+                </div>
+            </div>
+            <div class="column eight">
+                <img src="/assets/images/hero-basement-window.jpg">
+            </div>
+        </div>
+    </div>
+</div>
+<!--<hr>-->
+<div role="presentation" class="row">
+    <div class="spotlight oneFifth">
+        <h4>Aluminum Form</h4>
+        <img src="/assets/images/windows/wall-form.jpg">
+        <p>Extra durable fae sheet is totally enclosed by the extruded end rails and side rails, substantially reducing sheet peeling.</p>
+    </div>
+    <div class="spotlight oneFifth">
+        <h4>Aluminum Form Tie</h4>
+        <img src="/assets/images/windows/form-tie-small.jpg">
+        <p>Rugged cross members shed concrete, reducing weight-gain while reinforcing the face material for flatter poured walls.</p>
+    </div>
+    <div class="spotlight oneFifth">
+        <h4>Aluminum Form Hardware</h4>
+        <img src="/assets/images/windows/alum-form-hardware.jpg">
+        <p>Side rails feature redesigned bushings which distribute concrete load and extend bushing life.</p>
+    </div>
+    <div class="spotlight oneFifth">
+        <h4>Basement Window Systems</h4>
+        <img src="/assets/images/windows/basement-window.jpg">
+        <p>Optional Ready Pin attached hardware is durable, lightweight and increases labor productivity while reducing lost hardware expenses.</p>
+    </div>
+    <div class="spotlight oneFifth">
+        <h4>Dealer Inquiries Welcome</h4>
+        <img src="/assets/images/windows/form-hero-small.jpg">
+        <p>Interchangeable with other aluminum forming systems.</p>
+        <ul>
+            <li>Available in heights from 1' to 9'</li>
+            <li>36" x 8' 6-12 Weighs 84lbs.</li>
+        </ul>
+    </div>
+</div>
