a29c244b4c407ecac517f848b671920f908a531c
Ryan <iamryandawson@gmail.com>
2014-07-30 07:04:23 -0400

    Window Systems - 2nd tier of content

--- application/views/window-systems.php
+++ application/views/window-systems.php
@@ -3,8 +3,8 @@
         <div role="presentation" id="hero">
             <div class="column four">
                 <div>
-                    <h2 class="smaller">Snap-In-Place</h2>
-                    <h2 class="smaller" style="border:none">Window Systems</h2>
+                    <h2 style="line-height:1.25; padding-bottom:.35em">Snap-In-Place</h2>
+                    <h3>Window Systems</h3>
                     <h3 style="font-size: 1.5em">Safe and Secure Basement Window Systems</h3>
                 </div>
             </div>
@@ -14,35 +14,46 @@
         </div>
     </div>
 </div>
-<!--<hr>-->
+<hr>
+<div role="presentation" class="row" id="window-systems">
+    <div class="column six">
+        <h2>TUF-N-LITE&reg; Basement Window System</h2>
+        <p>The TUF-N-LITE® Snap-In-Place window system offers advantages of a high quality vinyl window with ease of our patented snap in installation process. Aluminum pouring buck forms a window sill and a integrated retainer system that allows the window to securely snap into   place. No drilling necessary and installation only takes seconds! Our quality windows are available in slider and hopper styles.</p>
+        <p>Call us today to learn more about these.</p>
+    </div>
+    <div class="column six">
+        <img src="/assets/images/windows/window-system-install.jpg" alt="Window System Installation Diagram">
+    </div>
+</div>
+<hr>
 <div role="presentation" class="row">
     <div class="spotlight oneFifth">
         <h4>Aluminum Form</h4>
         <img src="/assets/images/windows/wall-form.jpg">
-        <p>Extra durable fae sheet is totally enclosed by the extruded end rails and side rails, substantially reducing sheet peeling.</p>
+        <!--        <p>Extra durable fae sheet is totally enclosed by the extruded end rails and side rails, substantially reducing sheet peeling.</p>-->
     </div>
     <div class="spotlight oneFifth">
         <h4>Aluminum Form Tie</h4>
         <img src="/assets/images/windows/form-tie-small.jpg">
-        <p>Rugged cross members shed concrete, reducing weight-gain while reinforcing the face material for flatter poured walls.</p>
+        <!--        <p>Rugged cross members shed concrete, reducing weight-gain while reinforcing the face material for flatter poured walls.</p>-->
     </div>
     <div class="spotlight oneFifth">
         <h4>Aluminum Form Hardware</h4>
         <img src="/assets/images/windows/alum-form-hardware.jpg">
-        <p>Side rails feature redesigned bushings which distribute concrete load and extend bushing life.</p>
+        <!--        <p>Side rails feature redesigned bushings which distribute concrete load and extend bushing life.</p>-->
     </div>
     <div class="spotlight oneFifth">
         <h4>Basement Window Systems</h4>
         <img src="/assets/images/windows/basement-window.jpg">
-        <p>Optional Ready Pin attached hardware is durable, lightweight and increases labor productivity while reducing lost hardware expenses.</p>
+        <!--        <p>Optional Ready Pin attached hardware is durable, lightweight and increases labor productivity while reducing lost hardware expenses.</p>-->
     </div>
     <div class="spotlight oneFifth">
         <h4>Dealer Inquiries Welcome</h4>
         <img src="/assets/images/windows/form-hero-small.jpg">
-        <p>Interchangeable with other aluminum forming systems.</p>
-        <ul>
-            <li>Available in heights from 1' to 9'</li>
-            <li>36" x 8' 6-12 Weighs 84lbs.</li>
-        </ul>
+        <!--        <p>Interchangeable with other aluminum forming systems.</p>-->
+        <!--        <ul>-->
+        <!--            <li>Available in heights from 1' to 9'</li>-->
+        <!--            <li>36" x 8' 6-12 Weighs 84lbs.</li>-->
+        <!--        </ul>-->
     </div>
 </div>
